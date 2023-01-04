f7f7b45ae1c1ffcfd1bc33aeabdd5665705c7b48
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-01-03 21:11:41 -0300

    getting the data from the api, for the shopping cart

--- frontend/src/pages/Product/index.tsx
+++ frontend/src/pages/Product/index.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Container,Box1,Box2
 } from "./styles";
 const Product: React.FC = () => {
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
   return (
     <React.Fragment>
       <Container>
